1e86e17dc94c4279173cc70ee4013b086a4a9f92
Gokul <88606340+gokulgk-9402@users.noreply.github.com>
2022-06-21 14:41:27 +0530

    Delete June_2022 directory

--- June_2022/AddBinary_June16.py
+++
@@ -1,8 +0,0 @@
-# problem link -> https://leetcode.com/problems/add-binary/
-
-class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        i1 = int(a, 2)
-        i2 = int(b, 2)
-        
-        return str(bin(i1+i2))[2:]
--- June_2022/LengthofLastWord_June14.py
+++
@@ -1,5 +0,0 @@
-# problem link -> https://leetcode.com/problems/length-of-last-word/
-
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        return len(s.split()[-1])
